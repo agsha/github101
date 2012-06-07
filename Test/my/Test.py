8c8737a01f449b2f5d1229f22e0206f146d71056
Sharath Gururaj <e271828r@gmail.com>
2012-06-07 16:53:17 -0400

    adding tarring functionality

--- Test/my/Test.py
+++ Test/my/Test.py
@@ -8,9 +8,9 @@ import subprocess
 import re
 
 
-BITBUCKET_FOLDER = "./"
-AUTHORS_FILE = "users.txt"
-apps = {"survey":"git@bitbucket.org:edlab/apps-survey-sidekick.git"}
+BITBUCKET_FOLDER = os.path.abspath(".")
+AUTHORS_FILE = os.path.join(BITBUCKET_FOLDER, "users.txt")
+apps = {"django-lib":"git@bitbucket.org:edlab/apps-django-cas.git"}
 LINE_BUFFERED = 1
 # get a unique ID for this 
 i = 0
@@ -32,7 +32,7 @@ class NullConsumer(object):
 def exec_command(command, consumer = NullConsumer()):
     print("[[%s]]\n"%command)
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=LINE_BUFFERED)
-    proc.communicate()
+    proc.wait()
     consumer.begin()
     for line in proc.stdout:
         sys.stdout.write(line)
@@ -67,10 +67,12 @@ class RemoteAdded(object):
         if line.startswith("origin") == False:
             self.remote = False
    
-if __name__ == "__main__":
+def main():
     for app in apps :
-        folder = BITBUCKET_FOLDER + app
+        folder = os.path.join(BITBUCKET_FOLDER, app)
         os.chdir(BITBUCKET_FOLDER)
+        print "printing folderrr" + folder
+        print "prinnting git" + os.path.join(folder, ".git/")
         if not os.path.exists(folder) :
             exec_command("mkdir %s" % folder)
         os.chdir(folder)
@@ -78,8 +80,8 @@ if __name__ == "__main__":
         while True:
             missingAuthorConsumer = MissingAuthorConsumer()
             try:
-                if not os.path.exists(folder+"/.git"):
-                    proc = exec_command("git svn clone svn+ssh://sha@admin-edlab.tc.columbia.edu/var/svn/%s --authors-file=users.txt --no-metadata -s ."%(app), consumer=missingAuthorConsumer)
+                if not os.path.exists(os.path.join(folder, ".git/")) :
+                    proc = exec_command("git svn clone svn+ssh://sha@admin-edlab.tc.columbia.edu/var/svn/%s --authors-file=%s --no-metadata -s %s"%(app, AUTHORS_FILE, folder), consumer=missingAuthorConsumer)
                 else :
                     proc = exec_command("git svn fetch", consumer=missingAuthorConsumer)
             except Exception:
@@ -103,4 +105,20 @@ if __name__ == "__main__":
         if remoteAdded.remote != True:
             exec_command("git remote add origin %s"%apps[app])
         exec_command("git push origin --all")
+
+def tar():
+    tardir = os.path.join(os.path.abspath("/home/sha/"), "tar")
+    exec_command("mkdir %s" % tardir)
+    for dr in os.listdir("/var/trac/sites/"):
+        exec_command("tar -cvf %s %s"%(os.path.join(tardir, dr+".tar"), os.path.join("/var/trac/sites/", dr)))
     
+    
+    
+    
+if __name__ == '__main__':
+    method = 'main'
+    if len(sys.argv) > 1 :
+        method = sys.argv[1] 
+        globals()[sys.argv[1]]()
+    else:
+        main()    

7498af07d4595fc1f229423a9c7c80cdfc47d3d8
Sharath Gururaj <e271828r@gmail.com>
2012-06-21 09:31:37 -0400

    some bug fixes, cleanup (like deleting print messages)

--- my/Test.py
+++ my/Test.py
@@ -14,7 +14,26 @@ BITBUCKET_FOLDER = os.path.abspath(".")
 ERR = os.path.join(os.getcwd(), "err")
 OUT = os.path.join(os.getcwd(), "out")
 AUTHORS_FILE = os.path.join(BITBUCKET_FOLDER, "users.txt")
-apps = {"pundit_plus":"git@bitbucket.org:edlab/apps-pundit.git"}
+apps = {
+#        "configurations":"git clone git@bitbucket.org:edlab/unknown-configurations.git",
+#        "documentation": "git@bitbucket.org:edlab/unknown-documentation.git",
+#        "eddais":"git@bitbucket.org:edlab/apps-eddais.git",
+#        "edlabauth":"git@bitbucket.org:edlab/apps-edlab-cas.git",
+#        "identity": "git@bitbucket.org:edlab/apps-cas-main-code.git",
+#        "library": "git@bitbucket.org:edlab/library-library-website.git",
+#        #"mschool":"",
+#        #"netposse":"",
+#        #"oauth":"",
+#        #"ojs":"",
+#        #"slms":"",
+#        #"tclibrary":"",
+#        "tcrsearch":"git@bitbucket.org:edlab/publications-tc-record.git",
+#        "timesheet":"git@bitbucket.org:edlab/library-time-sheet-tool.git",
+#        "twitterSymposium":"git@bitbucket.org:edlab/library-twitter-symposium.git",
+#        "young_arts":"git@bitbucket.org:edlab/publications-young-arts-website.git",
+#        "NLT_CMS":"git@bitbucket.org:edlab/new-learning-times-cms.git",
+#        #"plugins":"",
+        }
 LINE_BUFFERED = 1
 # get a unique ID for this 
 i = 0
@@ -36,7 +55,7 @@ def exec_command(command, log=False, errorcheck=True):
     if errorcheck:
         if proc.returncode!=0:
             raise Exception("return code is not zero")
-        for l in open(ERR) :
+        for l in  open(ERR) :
             raise Exception("Error file not empty")
     return (open(OUT), open(ERR), proc.returncode)
 
@@ -58,9 +77,9 @@ def MissingAuthorConsumer(out, err, returncode):
     if (m == None and returncode!=0):
         raise Exception("[[script]] Command failed.")
     if m is not None:
-      return m.group("author")
+        return m.group("author")
     else:
-      return m
+        return m
 
 def RemoteAdded(out, err, returncode):
     for line in out:
@@ -70,53 +89,53 @@ def RemoteAdded(out, err, returncode):
 
 
 def _updateexcel(dir):
-  pass
+    pass
 
 
 def isNull(string):
-  if string == "":
-    return False
-  else: 
-    return True
+    if string == "":
+        return False
+    else: 
+        return True
   
 
 
         
 def main():
     for app in apps :
-        folder = os.path.join(BITBUCKET_FOLDER, app)
-        os.chdir(BITBUCKET_FOLDER)
-        print "printing folderrr" + folder
-        print "prinnting git" + os.path.join(folder, ".git/")
-        if not os.path.exists(folder) :
-            exec_command("mkdir %s" % folder, errorcheck = False)
-        os.chdir(folder)
-        open(AUTHORS_FILE, "a").close()
-        while True:
-            try:
+        print "======================================"
+        print "     Running script for %s"%app
+        print "======================================"
+        try:
+            folder = os.path.join(BITBUCKET_FOLDER, app)
+            os.chdir(BITBUCKET_FOLDER)
+            if not os.path.exists(folder) :
+                exec_command("mkdir %s" % folder, errorcheck = False)
+            os.chdir(folder)
+            open(AUTHORS_FILE, "a").close()
+            while True:
                 if not os.path.exists(os.path.join(folder, ".git/")) :
                     author = MissingAuthorConsumer(*exec_command("git svn clone svn+ssh://sha@admin-edlab.tc.columbia.edu/var/svn/%s --authors-file=%s --no-metadata -s %s"%(app, AUTHORS_FILE, folder), errorcheck=False,))
                 else :
                     author = MissingAuthorConsumer(*exec_command("git svn fetch", errorcheck = False))
-            except Exception:
-                pass
-            if author is None or len(author) == 0:
-                break
-
-            authorsFile = open(AUTHORS_FILE, "a")
-            authorsFile.writelines("%s = %s <edlabit+%s@tc.columbia.edu>\n"%(author, author, author))
-            authorsFile.close()
-        exec_command("cp -Rf .git/refs/remotes/tags/* .git/refs/tags/", errorcheck=False)
-        exec_command("find .git/refs/remotes/ -type f -not  -name  *trunk*   -exec mv {} .git/refs/heads/ \;", errorcheck=False)
-        good = RemoteAdded(*exec_command("git remote -v"))
-        if good != True:
-            exec_command("git remote add origin %s"%apps[app])
-            
-        val = exec_command("git push origin --all", errorcheck=False)
-        for l in val[1]:
-            if re.match("^[\w]*Everything up-to-date[\w]*$", l) is None:
-                raise Exception("Error occured in git push origin --all")
-        
+                if author is None or len(author) == 0:
+                    break
+    
+                authorsFile = open(AUTHORS_FILE, "a")
+                authorsFile.writelines("%s = %s <edlabit+%s@tc.columbia.edu>\n"%(author, author, author))
+                authorsFile.close()
+            exec_command("cp -Rf .git/refs/remotes/tags/* .git/refs/tags/", errorcheck=False)
+            exec_command("find .git/refs/remotes/ -type f -not  -name  *trunk*   -exec mv {} .git/refs/heads/ \;", errorcheck=False)
+            good = RemoteAdded(*exec_command("git remote -v"))
+            if good != True:
+                exec_command("git remote add origin %s"%apps[app])
+                
+            val = exec_command("git push origin --all", errorcheck=False)
+            for l in val[1]:
+                if re.match("^[\w]*Everything up-to-date[\w]*$", l) is None:
+                    raise Exception("Error occured in git push origin --all")
+        except Exception:
+            pass
 
 
 def tar():
@@ -136,4 +155,4 @@ if __name__ == '__main__':
         method = sys.argv[1] 
         globals()[sys.argv[1]]()
     else:
-        dummy()    
+        main()    

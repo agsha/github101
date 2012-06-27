80d1420db2c957b470a969190032421ddbb11dcf
Sharath Gururaj <sa2617@tc.columbia.edu>
2012-06-27 09:55:10 -0400

    working version with mocks

--- tests/install_httpd_test.py
+++ tests/install_httpd_test.py
@@ -9,18 +9,23 @@ from my.install_httpd import *
 from mock import MagicMock
 from mock import patch
 import my.install_httpd
+from mock import call
 
 l = []
 def mock_ex(*args):
     for arg in args:
         l.append(arg)
 
+mock = MagicMock()
 
-@patch("my.install_httpd.exec_command", mock_ex)
+@patch("my.install_httpd.exec_command", mock)
 def testDownloadHttpd():
     downloadHttpd()
-    assert len(l) == 2
-    assert l[0] == "curl -o httpd-2.4.2.tar.gz -L %s"%HTTPD_URL
+    expected = [call.method("curl -o httpd-2.4.2.tar.gz -L %s"%HTTPD_URL),
+                call.method("tar xvzf httpd-2.4.2.tar.gz -C %s"%DOWNLOAD_DIR)]
+    assert mock.mock_calls == expected
+    #assert len(l) == 2
+    #assert l[0] == "curl -o httpd-2.4.2.tar.gz -L %s"%HTTPD_URL
     
 def allTests():
     testDownloadHttpd()
